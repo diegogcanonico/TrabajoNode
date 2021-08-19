0008a4389adde8d308873f7d264b8a3c2c88e6b0
Juan I.Ordoqui <jeo.ordoqui@gmail.com>
2021-08-19 11:59:12 -0300

    Correccion

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,11 +17,13 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: "qfqbsiyomvmwda",
-    password: "902ba2b8849ab08cbc5f193bd2a1b0ebebbd7a7870e5c9af380a0883f7be8f66",
-    database: "d3tflev3vpiodi",
-    host: "ec2-52-3-130-181.compute-1.amazonaws.com",
-    dialect: "postgres",
-    port: 5432
+    url: DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        required: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };

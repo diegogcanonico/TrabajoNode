a3e4b909aefe0b00954181e4605e5fbe6c24547f
Juan I.Ordoqui <jeo.ordoqui@gmail.com>
2021-08-19 12:20:18 -0300

    Correccion

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,7 +17,11 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    url: DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
